40c489c32bb48571bfcd52983045f2ca5917c1ad
朱宇涵 <zhu341322@outlook.com>
2026-08-08 22:44:47 +0800

    updata

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import runpy
+import os
 import json
 import time
 import importlib
 from actions import roll_call, load_names
 
-print("\t欢迎来到随机点名系统")
+
+#自动创建空名单
+if not os.path.exists("name_list.json"):
+    with open("name_list.json", "w", encoding="utf-8") as f:
+        json.dump([], f)
+    print("已自动创建name_list.json")
+#进入主界面
+print("\n\t欢迎来到随机点名系统\n")
 while True:
     choose = int(input("\t输入“1”开始点名\n\t输入“2”管理名单\n\t输入“3”退出:\n"))
     if choose == 1:
         name_list = load_names()
-        roll_name = roll_call(name_list)
-        print(roll_name)
+        if not name_list:
+            print("\n\t名单为空，请先添加学生\n")
+        else:
+            roll_name = roll_call(name_list)
+            print(roll_name)
     elif choose == 2:
         runpy.run_module("manage")
     elif choose == 3:
-        print("再见\n")
+        print("\t再见\n")
         break
     else:
-        print("错误，请重新输入\n")
+        print("\n\t错误，请重新输入\n")
         time.sleep(0.5)
--- manage.py
+++ manage.py
@@ -1,17 +1,26 @@
 import json
 import importlib
 import time
-from actions import deng_ji, delete, show
+from actions import deng_ji, delete, show, load_names
+
+
 while True:
+    json_list = load_names()
     choose_manage = int(input("\t输入“1“”登记新学生\n\t输入“2”移除现有学生\n\t输入“3”显示现有学生\n\t输入“4”退回主界面\n"))
     if choose_manage == 1:
         new_name = input("登记新学生:")
         deng_ji(new_name)
     elif choose_manage == 2:
-        delete_name = input("删除学生:")
-        delete(delete_name)
+        if not json_list:
+            print("\n\t名单为空，请先添加学生\n")
+        else:
+            delete_name = input("删除学生:")
+            delete(delete_name)
     elif choose_manage == 3:
-        show()
+        if not json_list:
+            print("\n\t名单为空，请先添加学生\n")
+        else:
+            show()
     elif choose_manage == 4:
         break
     else:
